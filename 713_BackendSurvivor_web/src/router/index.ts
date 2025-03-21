dadf024a4b1ed14cca2955ef20eb3739f3c8ec10
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-21 22:29:34 +0700

    Modify routes

--- 713_BackendSurvivor_web/src/router/index.ts
+++ 713_BackendSurvivor_web/src/router/index.ts
@@ -1,15 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import StudentView from '../views/admin/student/StudentView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/login',
-      name: 'login-view',
-      component: LoginView,
-    },
     {
       path: '/admin-students',
       name: 'admin-students',
@@ -17,14 +11,6 @@ const router = createRouter({
       props: (route) => ({
         page: parseInt(route.query.page as string) || 1,
       }),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
     },
   ],
 })
