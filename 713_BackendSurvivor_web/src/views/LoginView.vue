1869aec4aee7417a897f3034edc22c717b605148
Phatcharaphon Munsaenlan <phatcharaphonmunsaenlan@Phatcharaphons-MacBook-Air.local>
2025-03-22 16:00:28 +0700

    LoginEdit1

--- 713_BackendSurvivor_web/src/views/LoginView.vue
+++ 713_BackendSurvivor_web/src/views/LoginView.vue
@@ -7,14 +7,14 @@
             alt="Your Company"
           />
           <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Student Advisor Management
           </h2>
         </div>
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form class="space-y-6" action="#" method="POST">
             <div>
               <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-                >Email address</label
+                >UserName</label
               >
               <div class="mt-2">
                 <input
@@ -51,15 +51,15 @@
                 type="submit"
                 class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Log in
               </button>
             </div>
           </form>
           <p class="mt-10 text-center text-sm text-gray-500">
-            Not a member?
+            you are student?
             {{ ' ' }}
             <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              >Try to register here</a
+              >Register</a
             >
           </p>
         </div>
