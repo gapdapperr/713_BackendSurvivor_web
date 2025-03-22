40766131f7036a751bf3d18d9dcae0b278034ad5
Phatcharaphon Munsaenlan <phatcharaphonmunsaenlan@Phatcharaphons-MacBook-Air.local>
2025-03-22 16:09:30 +0700

    LoginOnIcon
    ยังไม่มีไอคอนที่ถูกใจ แต่เอาไปใช้ต่อกได้เลย ได้ไอคอนถูกใจค่อยปรับใหม่

--- 713_BackendSurvivor_web/src/views/LoginView.vue
+++ 713_BackendSurvivor_web/src/views/LoginView.vue
@@ -2,9 +2,7 @@
       <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            class="mx-auto h-10 w-auto"
-            src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.d52e9897.svg"
-            alt="Your Company"
+            src="./Logo/task-management.png"
           />
           <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Student Advisor Management
