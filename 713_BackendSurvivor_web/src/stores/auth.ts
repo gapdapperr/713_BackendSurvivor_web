96fce8d6932ebfc4721d149ea57a1dfe04ba17d1
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-22 19:55:21 +0700

    Implement authentication system - Implement auth store with token management

--- 713_BackendSurvivor_web/src/stores/auth.ts
+++ 713_BackendSurvivor_web/src/stores/auth.ts
@@ -1,10 +1,21 @@
-import apiClient from '@/services/AxiosClient'
 import { defineStore } from 'pinia'
+import apiClient from '@/services/AxiosClient'
+import type { User } from '@/types'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: null as string | null,
+    user: null as User | null,
   }),
+  getters: {
+    currentUserName(): string {
+      return this.user?.username || ''
+    },
+    isAdmin(): boolean {
+      return this.user?.role.includes('ROLE_ADMIN') || false
+    },
+  },
+
   actions: {
     async login(email: string, password: string) {
       const response = await apiClient.post('/api/v1/auth/authenticate', {
@@ -12,7 +23,23 @@ export const useAuthStore = defineStore('auth', {
         password: password,
       })
       this.token = response.data.access_token
+      localStorage.setItem('token', this.token as string)
+      const meResponse = await apiClient.get('/api/v1/auth/me')
+      this.user = meResponse.data.user
+      localStorage.setItem('user', JSON.stringify(this.user))
+
       return response
+    },
+    logout() {
+      console.log('logout')
+      this.token = null
+      this.user = null
+      localStorage.removeItem('access_token')
+      localStorage.removeItem('user')
+    },
+    reload(token: string, user: User) {
+      this.token = token
+      this.user = user
     },
   },
 })
--- 713_BackendSurvivor_web/src/views/LoginView.vue
+++ 713_BackendSurvivor_web/src/views/LoginView.vue
@@ -1,76 +1,108 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-const username = ref('')
-const password = ref('')
+import InputText from '@/components/InputText.vue'
+import * as yup from 'yup'
+import { useField, useForm } from 'vee-validate'
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+
+const authStore = useAuthStore()
+const router = useRouter()
+const messageStore = useMessageStore()
+const validationSchema = yup.object({
+  // username: yup.string().required('The username is required'),
+  password: yup
+    .string()
+    .required('The password is required')
+    .min(5, 'The password must be at least 5 characters.'),
+})
+const { errors, handleSubmit } = useForm({
+  validationSchema,
+  initialValues: {
+    username: '',
+    password: '',
+  },
+})
+const { value: username } = useField<string>('username')
+const { value: password } = useField<string>('password')
+const onSubmit = handleSubmit(async (values: { username: string; password: string }) => {
+  try {
+    await authStore.login(values.username, values.password)
+    router.push({ name: 'test-login' })
+  } catch {
+    messageStore.updateMessage('Could not login')
+    setTimeout(() => {
+      messageStore.resetMessage()
+    }, 3000)
+  }
+})
 </script>
 
 <template>
-  <pre>
-    {{ username }}
-    {{ password }}
-  </pre>
-      <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            src="./Logo/task-management.png"
-          />
-          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Student Advisor Management
-          </h2>
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img
+        class="mx-auto h-10 w-auto"
+        src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.d52e9897.svg"
+        alt="Your Company"
+      />
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Sign in to your account
+      </h2>
+    </div>
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" @submit.prevent="onSubmit">
+        <div>
+          <label for="username" class="block text-sm font-medium leading-6 text-gray-900"
+            >Username</label
+          >
+          <div class="mt-2">
+            <InputText
+              id="username"
+              type="username"
+              v-model="username"
+              placeholder="Username"
+              :error="errors['username']"
+            />
+          </div>
         </div>
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form class="space-y-6" action="#" method="POST">
-            <div>
-              <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-                >UserName</label
-              >
-              <div class="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required="true"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <div class="flex items-center justify-between">
-                <label for="password" class="block text-sm font-medium leading-6 text-gray-900"
-                  >Password</label
-                >
-                <div class="text-sm">
-                  <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >Forgot password?</a
-                  >
-                </div>
-              </div>
-              <div class="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <button
-                type="submit"
-                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Log in
-              </button>
-            </div>
-          </form>
-          <p class="mt-10 text-center text-sm text-gray-500">
-            Are you student?
-            {{ ' ' }}
-            <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              >Register</a
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900"
+              >Password</label
             >
-          </p>
+            <div class="text-sm">
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500"
+                >Forgot password?</a
+              >
+            </div>
+          </div>
+          <div class="mt-2">
+            <InputText
+              type="password"
+              v-model="password"
+              placeholder="Password"
+              :error="errors['password']"
+              autocomplete="false"
+            />
+          </div>
         </div>
-      </div>
-    </template>
-    
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Not a member?
+        {{ ' ' }}
+        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >Try to register here</a
+        >
+      </p>
+    </div>
+  </div>
+</template>
