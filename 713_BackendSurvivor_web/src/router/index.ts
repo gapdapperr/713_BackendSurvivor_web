28d94ead84c8265e53d6fa6b7187b9d604ff9b64
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-22 20:19:35 +0700

    Update routes

--- 713_BackendSurvivor_web/src/router/index.ts
+++ 713_BackendSurvivor_web/src/router/index.ts
@@ -1,22 +1,49 @@
 import LoginView from '@/views/LoginView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '../views/LandingView.vue'
+import StudentView from '../views/dashboard-admin/student/StudentView.vue'
+import TestLoginView from '@/views/TestLoginView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'landing-view',
-      component: LandingView,
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/test-login',
+      name: 'test-login',
+      component: TestLoginView,
+    },
+    {
+      path: '/admin-students',
+      name: 'admin-students',
+      component: StudentView,
       props: (route) => ({
         page: parseInt(route.query.page as string) || 1,
       }),
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView,
+    },
+
+    {
+      path: '/404/:resource',
+      name: '404-resource-view',
+      component: NotFoundView,
+      props: true,
+    },
+
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView,
     },
   ],
 })
